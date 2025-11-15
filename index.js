f876f280c3b8d0fd8ec9b8da964abf45ef5a28f2
vikornienko <vikornienko76@gmail.com>
2025-11-15 23:09:48 +0500

    Добавлен драйвер для браузера Хром.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const { Builder, Browser } = require("selenium-webdriver");
 
 (async function helloSelenium() {
-    let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+    let driverFirefox = await new Builder().forBrowser(Browser.FIREFOX).build();
 
     try {
-        await driver.get("https://ya.ru");
-        const pageTitle = await driver.getTitle();
+        await driverFirefox.get("https://ya.ru");
+        const pageTitle = await driverFirefox.getTitle();
         console.log(pageTitle);
     } finally {
-        await driver.quit();
+        await driverFirefox.quit();
+    }
+
+    let driverChrome = await new Builder().forBrowser(Browser.CHROME).build();
+
+    try {
+        await driverChrome.get("https://google.com");
+        const pageTitle = await driverChrome.getTitle();
+        console.log(pageTitle);
+    } finally {
+        await driverChrome.quit();
     }
         
 }) ();
